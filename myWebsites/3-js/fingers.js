3820a7234e9377d206d5bfb59b4b85a7bce62cef
Rich Green <rmgreenjun@gmail.com>
2017-03-16 18:42:28 -0400

    properly attached button click event & made external js file

--- myWebsites/3-js/fingers.js
+++ myWebsites/3-js/fingers.js
@@ -1,12 +1,17 @@
 
-function makeRandomInt(min, max){
-	return math.random();
-}
-	
-	var userGuess = document.getElementById("userGuess").value;
-	var randomNum = document.getElementById("randomBtn").onclick = makeRandomInt(0,5);
-	if (userGuess==randomNum.value) {
-		document.getElementById("resultPar").innerHTML = "You guessed right!";
-	} else {
-		document.getElementById("resultPar").innerHTML = "Wrong guess again!";
-	}
+		 var randomBtn = function(){
+			var userGuess = parseInt(document.getElementById("userGuess").value);
+			var randomNum = makeRandomInt();
+			if (userGuess == randomNum) {
+				document.getElementById("resultPar").innerHTML = "You guessed right!";
+			}else {
+				document.getElementById("resultPar").innerHTML = 
+				"Wrong. Guess again! The number was " +randomNum;
+			}
+		};	    	
+		
+		document.getElementById("randomBtn").onclick = randomBtn;
+		
+		function makeRandomInt(){
+			return Math.floor(Math.random() *6);
+		}
